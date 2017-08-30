4113f5005fabfabc0935e241b09ccbd1170a3e01
Justin Phillips <justinphil02@yahoo.com>
2017-08-30 00:07:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,16 @@
 def fizzbuzz(num)
   if(num % 3 == 0)
     print "Fizz"
+    x="Fizz"
+      if(num % 5 == 0)
+        print "Buzz"
+        x="FizzBuzz"
+      end
   end
 
   if(num % 5 == 0)
     print "Buzz"
-
+    x="Buzz"
+    
   end
 end
